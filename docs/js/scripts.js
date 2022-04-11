143436341c0898e577d5798abc8baec058c5c10e
AbrahamAllen <100115719+AbrahamAllen@users.noreply.github.com>
2022-04-11 13:43:17 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -13,12 +13,13 @@ function write(txt, add=false){
 const elemList = ['fire', 'water', 'earth', 'air', 'acid', 'electric'];
 const weakDict = new Object(); weakDict['fire'] = 'water, air';weakDict['water'] = 'electric, air';weakDict['earth'] = 'fire, acid'; weakDict['air'] = 'earth, electric '; weakDict['acid'] = 'water'; weakDict['electric'] = 'earth';
 const strongDict = new Object(); strongDict['fire'] = 'electric, fire'; strongDict['water'] = 'fire, acid'; strongDict['earth'] = 'electric, air'; strongDict['air'] = 'acid'; strongDict['electric'] = 'electric'; strongDict['acid'] = 'earth, acid';
+const statusDict = new Object(); statusDict['fire'] = 'burn'; statusDict['water'] = 'waterlog'; statusDict['earth'] = 'spikes'; statusDict['air'] = 'unsteady'; statusDict['acid'] = 'poisin'; statusDict['electric'] = 'paralasys';
 const shapeList = ['snake', 'wolf', 'horse', 'bear', 'cat'];
 const mapCol = new Object();
 
 const moveDict = new Object();
 moveDict['wolf'] = ['scratch <br> /3/90', 'bite <br> /5/80', 'pounce <br> /10/70']; moveDict['snake'] = ['constrict <br> /4/90', 'bite <br> /5/80', 'swallow <br> /100/40']; moveDict['horse'] = ['kick <br> /4/90', 'bite <br> /5/80', 'charge <br> /7/80']; moveDict['bear'] = ['claw <br> /5/80', 'charge <br> /7/80', 'maul <br> /10/70']; moveDict['cat'] = ['bite <br> /5/80', 'slash <br> /7/80', 'pounce <br> /10/70'];
-moveDict['fire'] = ['fireball <br> /5/90', 'flamethrower <br> /10/80']; moveDict['water'] = ['water blast <br> /4/100', 'drown <br> /50/50']; moveDict['air'] = ['air blade <br> /3/100', 'windstorm <br> /10/70']; moveDict['earth'] = ['rock throw <br> /5/90', 'earthquake <br> /10/100']; moveDict['acid'] = ['spit <br> /5/80', 'toxins <br> /30/60']; moveDict['electric'] = ['shock <br> /5/100', 'thunder <br> /10/80'];
+moveDict['fire'] = ['fireball <br> /5/90/5', 'flamethrower <br> /10/80/10']; moveDict['water'] = ['water blast <br> /4/100/5', 'drown <br> /50/50']; moveDict['air'] = ['air blade <br> /3/100/3', 'windstorm <br> /10/70/7']; moveDict['earth'] = ['rock throw <br> /5/90/10', 'earthquake <br> /10/100']; moveDict['acid'] = ['spit <br> /5/80/10', 'toxins <br> /30/60']; moveDict['electric'] = ['shock <br> /5/100/3', 'thunder <br> /10/80/5'];
 
 
 class Monster{
@@ -35,20 +36,28 @@ class Monster{
 		if(scale > 3){this.moves.push(moveDict[this.type.elem][0])};
 		if(scale > 5){this.moves.push(moveDict[this.type.shape][2])};
 		if(scale > 7){this.moves.push(moveDict[this.type.elem][1])};
+		
+		this.item = 'none';
 	}
 	
 	//comabt functions
 	attack(move, target){
-	if(this.stats.hp > 0){	
+	if(this.stats.hp > 0 && this.longStatus()){	
 		if(target.stats.hp <= 0){write('<br>enemy killed <br> Click to continue'); return};
+	
 		let txt = move.innerText.split('/'); console.log(txt[0]); let amt = parseInt(txt[1]);
 
 		if(random(0,100)<txt[2]){ write('<br>'+this.id+ ' used '+txt[0], true); target.damage(amt*this.stats.atk, this.type.elem)}
 		else{write('<br>'+this.id +' missed', true)}
+		
+		if(!txt[3]){txt.push('3')};
+		if(random(25,100) < this.stats.affinity*parseInt(txt[3]) && target.stats.status == 'none'){target.stats.status = this.type.elem; write('<br>' +statusDict[this.type.elem] + ' applied', true); this.applyStatus(statusDict[this.type.elem])}
 	}}
 	
 	damage(amt, elem){
-		let dmg = amt;
+		console.log(Math.floor(this.stats.killcount/25));
+		let dmg = amt+Math.floor(this.stats.killcount/25);
+		if(this.stats.status == 'earth'){dmg+=5; write('<br>spiked, bonus damage', true)};
 		if(this.type.weak.includes(elem)){dmg+=dmg; write('<br> super effective', true)};
 		if(this.type.strong.includes(elem)){Math.round(dmg-=(dmg/2)); write('<br> not very effective', true)};
 		dmg = dmg-this.stats.def;
@@ -67,7 +76,7 @@ class Monster{
 		document.getElementById(this.id).remove();
 		if(this.id == 'player'){user.party.splice(this.slot, 1); document.getElementById('playermoves').remove();if(user.party.length == 0){gameOver()}; user.party[0].build(); user.activeMonster = user.party[0]; user.slotOrg()};
 		if(this.id == 'enemy'){user.activeMonster.stats.killcount++; rewards(Math.ceil(this.stats.lvl/2)); user.enemy = false};
-		if(this.id == 'boss'){rewards(this.stats.lvl); user.enemy = false};
+		if(this.id == 'boss'){user.activeMonster.stats.killcount++; rewards(this.stats.lvl); user.enemy = false};
 		
 		console.log(user.party);
 	}
@@ -82,15 +91,16 @@ class Monster{
 		this.stats.atk += this.stats.growth[1];
 		this.stats.def += this.stats.growth[2];
 		this.stats.spd += this.stats.growth[3];
+		this.stats.affinity += this.stats.growth[4];
 		this.stats.lvl++;
 		
 		this.stats.hp = this.stats.maxhp;
 		
 		
 		if(this.stats.lvl == 2 && !this.moves.includes(moveDict[this.type.shape][1])){this.moves.push(moveDict[this.type.shape][1])};
-		if(this.stats.lvl == 4 && !this.moves.includes(moveDict[this.type.shape][1])){this.moves.push(moveDict[this.type.elem][0])};
-		if(this.stats.lvl == 6 && !this.moves.includes(moveDict[this.type.shape][1])){this.moves.push(moveDict[this.type.shape][2])};
-		if(this.stats.lvl == 8 && !this.moves.includes(moveDict[this.type.shape][1])){this.moves.push(moveDict[this.type.elem][1])};
+		if(this.stats.lvl == 4 && !this.moves.includes(moveDict[this.type.elem][0])){this.moves.push(moveDict[this.type.elem][0])};
+		if(this.stats.lvl == 6 && !this.moves.includes(moveDict[this.type.shape][2])){this.moves.push(moveDict[this.type.shape][2])};
+		if(this.stats.lvl == 8 && !this.moves.includes(moveDict[this.type.elem][1])){this.moves.push(moveDict[this.type.elem][1])};
 	
 		
 		alert('LEVEL UP');
@@ -138,6 +148,13 @@ class Monster{
 				user.openParty();
 			}
 			document.getElementById('playermoves').appendChild(swap);
+			
+			let bag = document.createElement('div');
+			bag.className = 'button';
+			bag.style.backgroundColor = 'brown';
+			bag.innerHTML = 'bag';
+			bag.onclick = function(){user.openBag()};
+			document.getElementById('playermoves').appendChild(bag);
 		};
 	}
 	
@@ -149,12 +166,36 @@ class Monster{
 		txt += 'attack '+this.stats.atk+'<br>';
 		txt += 'defense '+this.stats.def+'<br>';
 		txt += 'speed '+this.stats.spd+'<br>';
+		txt += 'affinity '+this.stats.affinity+'<br>';
 		txt += 'exp '+this.stats.exp+'<br>';
 		txt += 'lvl '+this.stats.lvl+'<br>';
 		txt += this.type.elem;
 		
 		return txt; 
 		
+	}
+	
+	equip(item){
+		item.apply(this);
+	}
+	
+	longStatus(){
+		switch(this.stats.status){
+			case 'fire': this.stats.hp-=2; write('<br>burn damage applied', true); break;
+			case 'acid' : this.stats.hp-=Math.floor(this.stats.maxhp/8); write('<br>poisin damage applied', true);break;
+			case 'electric' : if(random(0, 100)> 60){write('<br>shocked! cant move', true); return false}else{break};
+			default : break;
+		}
+		document.getElementById(this.id).innerHTML = this.stats.hp;
+		if(this.stats.hp <= 0){this.kill(); return false}
+		return true;
+	}
+	
+	applyStatus(status){
+		switch(status){
+			case 'waterlog': this.stats.atk-=2; this.stats.def-=2; break;
+			default: break;
+		}
 	}
 }
 
@@ -173,6 +214,7 @@ class Stats{
 		this.atk = 2;
 		this.def = 2;
 		this.spd = 5;
+		this.affinity = 1;
 		
 		this.exp = 0;
 		this.lvl = scale;
@@ -181,20 +223,22 @@ class Stats{
 		
 		this.killcount = 0;
 		
+		this.status = 'none';
+		
 		switch(shape){
-			case 'snake': this.def+=2; this.growth = [10,1,3,0]; break; 
-			case 'wolf': this.spd++; this.atk++; this.growth = [10, 2, 1, 1]; break; 
-			case 'horse': this.spd++; this.def++; this.growth = [10, 1, 2, 2]; break; 
-			case 'bear': this.def++; this.maxhp+=5; this.growth = [15, 1, 2, 0]; break;
-			case 'cat': this.spd++; this.atk++; this.growth = [10, 2, 0, 2]; break; 
+			case 'snake': this.def+=2; this.growth = [10,1,3,0,3]; break; 
+			case 'wolf': this.spd++; this.atk++; this.growth = [10, 2, 1, 1,2]; break; 
+			case 'horse': this.spd++; this.def++; this.growth = [10, 1, 2, 2,1]; break; 
+			case 'bear': this.def++; this.maxhp+=5; this.growth = [15, 1, 2, 0,1]; break;
+			case 'cat': this.spd++; this.atk++; this.growth = [10, 2, 0, 2,2]; break; 
 		}
 		
 		switch(elem){
-			case 'fire': this.growth[1]+=2; break;
+			case 'fire': this.growth[1]+=2; this.growth[4]+=1; break;
 			case 'water': this.growth[0]+=5; this.growth[3]++; break;
 			case 'earth': this.growth[2]++; this.growth[0]+=5; break;
-			case 'air': this.growth[3]++; this.growth[2]++; break;
-			case 'acid': this.growth[1]++; this.growth[2]++; break;
+			case 'air': this.growth[3]++; this.growth[2]++; this.growth[4]+=1; break;
+			case 'acid': this.growth[1]++; this.growth[2]++; this.growth[4]+=1; break;
 			case 'electric': this.growth[1]++; this.growth[3]++; break;
 		}
 		
@@ -204,6 +248,7 @@ class Stats{
 			this.atk += this.growth[1];
 			this.def += this.growth[2];
 			this.spd += this.growth[3];
+			this.affinity += this.growth[4];
 			i++;
 		}
 		this.hp = this.maxhp;
@@ -230,6 +275,7 @@ class User{
 		
 		this.inbattle = false;
 		this.level = 0;
+		this.bag = [];
 	}
 	
 	locate(x,y){
@@ -254,11 +300,11 @@ class User{
 	col(){
 		for(let encounter of Object.values(mapCol)){
 			if(this.x > encounter[0] && this.x < encounter[0]+encounter[2] && this.y > encounter[1] && this.y < encounter[1]+encounter[3]){
-				
+				if(encounter[4].id.includes('biome')){user.berryCheck(encounter[4])};
 				if(encounter[4].type == 'heal'){popup('heal your monster?', 'heal', encounter[4].id); return;};
 				if(encounter[4].type == 'level'){user.party[0].levelUp(); document.getElementById(encounter[4].id).remove(); delete mapCol[encounter[4].id]; return};
 				if(encounter[4].type == 'portal'){popup('go to next floor?', 'portal', undefined); return};
-				if(random(0, 1000) > 800){
+				if(random(0, 1000) > 800 && !encounter[4].id.includes('biome')){
 					user.dx = 0; user.dy = 0;
 					encounter[4].shrink();
 					battle(user.party[0], encounter[4])
@@ -300,6 +346,38 @@ class User{
 		this.activeMonster = this.party[0];
 	}
 	
+	
+	openBag(){
+		let bag = document.createElement('div');
+		bag.id = 'bag';
+		bag.onclick = function(){user.closeBag()};
+		document.getElementById('battlefield').appendChild(bag);
+		let i = 0;
+		for(let item of user.bag){
+			let div = document.createElement('div');
+			div.className = 'button '+item.type;
+			div.innerHTML = item.amt;
+			div.id = i; i++;
+			div.onclick = function(){
+				let item = user.bag[parseInt(this.id)];
+				user.bag.splice(parseInt(this.id), 1);
+				user.activeMonster.equip(item);
+			}
+			document.getElementById('bag').appendChild(div);
+		}
+		
+	}
+	
+	closeBag(){
+		document.getElementById('bag').remove();
+	}
+	
+	berryCheck(encounter){
+	if(random(1000, 10000) > 9500 && encounter.berries > 0){
+		this.bag.push(new Berry(random(10, 10*this.level), encounter.elem));
+		popup('found berry!', 'berry', undefined);
+		encounter.berries--;
+	}}
 	
 }
 
@@ -348,10 +426,41 @@ function popup(txt, id, interaction){
 		case 'portal': yes.onclick = function(){nextFloor();  document.getElementById('popup').remove()}; break;
 	}
 	
+	if(id == 'berry'){
+		div.onclick = function(){
+			setTimeout(function(){user.inbattle = false}, 100); 
+			document.getElementById('popup').remove()
+			}
+			document.getElementById('inputHandle').appendChild(div);
+			return
+		}
 		
 	document.getElementById('inputHandle').appendChild(div);
 	document.getElementById('popup').appendChild(yes);
 	document.getElementById('popup').appendChild(no);
+}
+
+//ITEMS 
+
+
+class Berry {
+	constructor(amt, type){
+		this.amt = amt;
+		this.type = type;
+	}
+	apply(monster){
+		console.log(monster);
+		monster.stats.hp+=10;
+		if(monster.stats.hp > monster.stats.maxhp){monster.stats.hp = monster.stats.maxhp};
+		document.getElementById(monster.id).innerHTML = monster.stats.hp;
+		write('used berry, healed for ' + this.amt);
+		if(this.type == monster.stats.status){
+			monster.stats.status = 'none';
+			if(this.type == 'water'){monster.stats.atk+=2; monster.stats.def+=2};
+			write('<br>cured status affliction', true);
+		}
+	}		
+	
 }
 
 
@@ -359,7 +468,7 @@ function popup(txt, id, interaction){
 //encounter creation and addition
 class Biome{
 	constructor(x,y,elem = elemList[random(0, elemList.length-1)]){
-		this.id = random(1000, 9999);
+		this.id = random(1000, 9999)+'biome';
 		
 		this.w = 1000;
 		this.h = 1000;
@@ -368,6 +477,7 @@ class Biome{
 		this.y = y;
 		
 		this.elem = elem;
+		this.berries = random(0, 3);
 	}
 	
 	init(){
@@ -381,6 +491,8 @@ class Biome{
 		div.className = this.elem + 'border';
 		document.getElementById('map').appendChild(div);
 		
+		
+		mapCol[this.id] = [this.x, this.y, this.w, this.h, this];
 		this.makeEncounter();
 	}
 	
@@ -539,7 +651,8 @@ function battle(player, encounter){
 	div.id = 'battlefield';
 	div.className = encounter.elem+'border';	
 	div.style.left = (user.x-200).toString()+'px';
-	div.style.top = (user.y-200).toString()+'px';
+	if(user.y < 200){div.style.top = (200).toString()+'px'}
+	else{div.style.top = (user.y-200).toString()+'px'};
 	document.body.appendChild(div);
 	
 	player.build();
@@ -574,13 +687,16 @@ function rewards(amt){
 	capture.className = 'button';
 	capture.innerHTML = 'Capture';
 	capture.onclick = function(){
+	if(user.party.length < 3){
 		user.killedMonster.stats.hp = user.killedMonster.stats.maxhp;
 		if(user.killedMonster.id == 'boss'){user.killedMonster.stats.lvl=0};
 		user.killedMonster.id = 'player';	
 		user.killedMonster.slot = user.party.length;
 		user.party.push(user.killedMonster); 
 		user.killedMonster = undefined; 
-		removeBattlefield()};
+		removeBattlefield()}
+	else{write('cannot capture <br> too many monsters in party')}};
+
 	document.getElementById('battlefield').appendChild(capture); 
 }
 
@@ -735,6 +851,7 @@ function chooseStarter(){
 		makeMap();
 		user.activeMonster = user.party[0];
 		user.party[0].stats.lvl = 1;
+		user.bag.push(new Berry(10, user.activeMonster.type.elem));
 	}}
 	
 	document.getElementById('chooseStarter').appendChild(done);
